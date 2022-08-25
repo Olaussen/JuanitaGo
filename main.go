b08f724ae0871de885246f222f9ec1d74cd1b1ad
Hauk Aleksander Olaussen <olaussen.hauk@gmail.com>
2022-08-25 17:29:16 +0200

    Initial commit for JuanitaGo with removed extra testcommands

--- main.go
+++ main.go
@@ -8,13 +8,12 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 var (
-	GuildID        = flag.String("guild", "", "Test guild ID. If not passed - bot registers commands globally")
+	GuildID        = flag.String("guild", "", "Leave blank to register global commands")
 	BotToken       = flag.String("token", utils.GetEnvironmentVariableByKey("BOT_TOKEN"), "Bot access token")
 	RemoveCommands = flag.Bool("rmcmd", true, "Remove all commands after shutdowning or not")
 )
@@ -38,15 +37,6 @@ const integerOptionMinValue = 1.0
 var commands = GetCommandConfig()
 
 var commandHandlers = map[string]func(session *discordgo.Session, interaction *discordgo.InteractionCreate){
-	"basic-command": func(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
-		session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: "Hey there! Congratulations, you just executed your first slash command",
-			},
-		})
-	},
-
 	"test": func(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
 		options := utils.ExtractOptions(interaction)
 		messageArguments := make([]interface{}, 0, len(options))
@@ -64,48 +54,6 @@ var commandHandlers = map[string]func(session *discordgo.Session, interaction *d
 			Data: &discordgo.InteractionResponseData{
 				Embeds: utils.PlayEmbed(*searchResult),
 			},
-		})
-	},
-
-	"followups": func(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
-		// Followup messages are basically regular messages (you can create as many of them as you wish)
-		// but work as they are created by webhooks and their functionality
-		// is for handling additional messages after sending a response.
-
-		session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				// Note: this isn't documented, but you can use that if you want to.
-				// This flag just allows you to create messages visible only for the caller of the command
-				// (user who triggered the command)
-				Flags:   discordgo.MessageFlagsEphemeral,
-				Content: "Surprise!",
-			},
-		})
-		msg, err := session.FollowupMessageCreate(interaction.Interaction, true, &discordgo.WebhookParams{
-			Content: "Followup message has been created, after 5 seconds it will be edited",
-		})
-		if err != nil {
-			session.FollowupMessageCreate(interaction.Interaction, true, &discordgo.WebhookParams{
-				Content: "Something went wrong",
-			})
-			return
-		}
-		time.Sleep(time.Second * 5)
-
-		content := "Now the original message is gone and after 10 seconds this message will ~~self-destruct~~ be deleted."
-		session.FollowupMessageEdit(interaction.Interaction, msg.ID, &discordgo.WebhookEdit{
-			Content: &content,
-		})
-
-		time.Sleep(time.Second * 10)
-
-		session.FollowupMessageDelete(interaction.Interaction, msg.ID)
-
-		session.FollowupMessageCreate(interaction.Interaction, true, &discordgo.WebhookParams{
-			Content: "For those, who didn't skip anything and followed tutorial along fairly, " +
-				"take a unicorn :unicorn: as reward!\n" +
-				"Also, as bonus... look at the original interaction response :D",
 		})
 	},
 }
@@ -146,14 +94,6 @@ func main() {
 
 	if *RemoveCommands {
 		log.Println("Removing commands...")
-		// // We need to fetch the commands, since deleting requires the command ID.
-		// // We are doing this from the returned commands on line 375, because using
-		// // this will delete all the commands, which might not be desirable, so we
-		// // are deleting only the commands that we added.
-		// registeredCommands, err := s.ApplicationCommands(s.State.User.ID, *GuildID)
-		// if err != nil {
-		// 	log.Fatalf("Could not fetch registered commands: %v", err)
-		// }
 
 		for _, v := range registeredCommands {
 			err := session.ApplicationCommandDelete(session.State.User.ID, *GuildID, v.ID)
@@ -163,5 +103,5 @@ func main() {
 		}
 	}
 
-	log.Println("Gracefully shutting down.")
+	log.Println("Juanita shutting down.")
 }
